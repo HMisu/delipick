87b204d80fd688e1d6007aafeaac950ce3f50709
HMisu <misuhan33@gmail.com>
2025-05-27 17:00:09 +0900

    🎉 init: delipick 프로젝트 초기 세팅

---
+++ eureka-server/src/test/java/com/delipick/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delipick.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
