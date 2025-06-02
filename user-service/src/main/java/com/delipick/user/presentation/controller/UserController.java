4c0464d39047854f0da3902a2155b9ef1be0b1be
HMisu <misuhan33@gmail.com>
2025-06-02 11:29:16 +0900

    :sparkles: feat: 인증 및 사용자 관리 기본 구조 구현 - AuthController, UserController 생성 - AuthService, UserService 기본 기능 추가 - LoginRequest, SignupRequest, UserDto, UserResponse DTO 작성

---
+++ user-service/src/main/java/com/delipick/user/presentation/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.delipick.user.presentation.controller;
+
+import com.delipick.user.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+}
