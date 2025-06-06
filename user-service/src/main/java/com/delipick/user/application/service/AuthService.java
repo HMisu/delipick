461a6198e361cdcd649bae073b0816f2a7cc1076
HMisu <misuhan33@gmail.com>
2025-06-06 22:34:13 +0900

    :sparkles: feat: 회원가입 기능 구현

--- user-service/src/main/java/com/delipick/user/application/service/AuthService.java
+++ user-service/src/main/java/com/delipick/user/application/service/AuthService.java
@@ -1,9 +1,49 @@
 package com.delipick.user.application.service;
 
+import com.delipick.user.application.dto.UserDto;
+import com.delipick.user.domain.model.User;
+import com.delipick.user.domain.repository.UserRepository;
+import com.delipick.user.presentation.request.SignupRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public UserDto signup(@Valid SignupRequest request) {
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        checkEmailDuplication(request.email());
+        checkPhoneDuplication(request.phone());
+
+        User user = User.create(
+                request.email(),
+                encodedPassword,
+                request.phone(),
+                request.birthdate(),
+                request.address());
+
+        User savedUser = userRepository.save(user);
+        return UserDto.of(savedUser);
+    }
+
+    private void checkEmailDuplication(String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+    }
+
+    private void checkPhoneDuplication(String phone) {
+        if (userRepository.findByPhone(phone).isPresent()) {
+            throw new IllegalArgumentException("이미 가입된 전화번호입니다.");
+
+        }
+    }
 }
--- user-service/src/main/java/com/delipick/user/presentation/controller/AuthController.java
+++ user-service/src/main/java/com/delipick/user/presentation/controller/AuthController.java
@@ -1,7 +1,14 @@
 package com.delipick.user.presentation.controller;
 
+import com.delipick.user.application.dto.UserDto;
 import com.delipick.user.application.service.AuthService;
+import com.delipick.user.common.dto.ApiResponse;
+import com.delipick.user.presentation.request.SignupRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<UserDto>> signup(@Valid @RequestBody SignupRequest signupRequest) {
+        UserDto createdUser = authService.signup(signupRequest);
+
+        return ResponseEntity.ok(ApiResponse.success(createdUser));
+    }
 }
