902def642c6e1d41234eb0af18bc86ca32bebb71
HMisu <misuhan33@gmail.com>
2025-06-08 21:05:59 +0900

    :sparkles: feat: UserRepository에 findByEmailAndIsDeletedFalse 메서드 추가

--- user-service/src/main/java/com/delipick/user/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/delipick/user/domain/repository/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 public interface UserRepository {
     Optional<User> findById(Long id);
 
+    Optional<User> findByEmailAndIsDeletedFalse(String username);
+
     Optional<User> findByEmail(String email);
 
     Optional<User> findByPhone(String phone);
