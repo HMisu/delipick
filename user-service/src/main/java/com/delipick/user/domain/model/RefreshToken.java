3f0b4530eee8e363a98060fd33455b862fcd4b4e
HMisu <misuhan33@gmail.com>
2025-06-12 00:31:13 +0900

    :recycle: refactor : Redis에 저장되는 RefreshToken 유효기간 14일로 연장

--- user-service/src/main/java/com/delipick/user/domain/model/RefreshToken.java
+++ user-service/src/main/java/com/delipick/user/domain/model/RefreshToken.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @Setter
-@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 3)
+@RedisHash(value = "refreshToken", timeToLive = 60 * 60 * 24 * 14)
 public class RefreshToken {
     @Id
     private String refreshToken;
