02a700cc199fce3a270e82906043f40ffa5bf0c5
HMisu <misuhan33@gmail.com>
2025-06-12 21:56:26 +0900

    :sparkles: feat: 에러 코드 ENUM에 블랙리스트 관련 코드 추가 - LOGOUT_TOKEN_BLACKLISTED 에러 코드 추가 - 블랙리스트 토큰 감지 시 해당 코드로 응답

--- user-service/src/main/java/com/delipick/user/presentation/exception/enums/ErrorCode.java
+++ user-service/src/main/java/com/delipick/user/presentation/exception/enums/ErrorCode.java
@@ -12,7 +12,7 @@ public enum ErrorCode {
     USER_NOT_FOUND(404, "E4041", "해당 사용자를 찾을 수 없습니다."),
     REFRESH_TOKEN_NOT_FOUND(404, "E4042", "저장된 리프레시 토큰이 없거나 로그아웃된 상태입니다."),
     LOGOUT_TOKEN_SAVE_FAILURE(500, "E5002", "로그아웃 토큰 저장에 실패했습니다."),
-    LOGOUT_TOKEN_SAVED(200, "S2001", "로그아웃 토큰이 블랙리스트에 저장되었습니다."),
+    LOGOUT_TOKEN_BLACKLISTED(401, "E4013", "로그아웃된 토큰으로 접근할 수 없습니다."),
     INTERNAL_ERROR(500, "E5001", "서버 내부 오류입니다.");
 
     private final int status;
